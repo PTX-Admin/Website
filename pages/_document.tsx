775faf7b38f9f4ed1284a9fc9fe9a82d4bb3a54a
kinsyu <miguelangelrm31@gmail.com>
2022-08-11 17:38:17 +0100

    is an

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class Document extends NextDocument {
           <meta property="og:image" content="/logo512.png" />
           <meta
             property="og:description"
-            content="ProtocolX an is Auto-Compouding Protocol
+            content="ProtocolX is an Auto-Compouding Protocol
             $PTX begins as a 1.77% daily auto-rebasing token. What does that mean? Once you buy the token, it will begin to automatically rebase in your wallet. No work to do to make the magic happen!
             Just by simply holding the $PTX token in your wallet, you will receive rebases every 30 minutes that are directly and immediately added to your wallet (no claiming required)."
           />
