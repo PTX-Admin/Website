fb3e35f59b568e61db008502237f7de0c115e2c3
kinsyu <miguelangelrm31@gmail.com>
2022-08-15 17:22:07 +0100

    removed particles

--- components/HomeHeader/HomeHeader.tsx
+++ components/HomeHeader/HomeHeader.tsx
@@ -1,10 +1,6 @@
 import React, { useCallback, useEffect } from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
 import { Button, Image } from '@chakra-ui/react';
 import styles from './HomeHeader.module.css';
-import { Engine } from 'tsparticles-engine/types/engine';
-import { Container } from 'tsparticles-engine';
 import { useRouter } from 'next/router';
 import shield from '../../Assets/home/header/shield.png';
 import icon from '../../Assets/home/header/icon.png';
@@ -13,111 +9,8 @@ import circle from '../../Assets/home/header/circle.png';
 function Hero() {
   const router = useRouter();
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      const item = document.querySelector('#tsparticles canvas');
-      if (!item) return;
-      item.attributes[0].value =
-        'position: absolute !important; width: 100% !important; height: 100% !important; pointer-events: none; z-index: 0 !important; top: 0px !important; left: 0px !important;';
-    }, 500);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    await console.log(container);
-  }, []);
-
   return (
     <div className="cont">
-      <div className={styles.heroParticles}>
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: 'transparent',
-              },
-            },
-            fpsLimit: 30,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: 'push',
-                },
-                onHover: {
-                  enable: true,
-                  mode: 'repulse',
-                },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 100,
-                  duration: 0.8,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: '#c00000',
-              },
-              links: {
-                color: '#c00000',
-                distance: 150,
-                enable: true,
-                opacity: 0.7,
-                width: 1.2,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                direction: 'none',
-                enable: true,
-                outModes: {
-                  default: 'bounce',
-                },
-                random: false,
-                speed: 4,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 100,
-              },
-              opacity: {
-                value: 0.9,
-              },
-              shape: {
-                type: 'circle',
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-          style={{ position: 'absolute' }}
-        />
-      </div>
       <div className={styles.hero}>
         <div data-aos="fade-right" data-aos-delay={200} className={styles.leftHero}>
           <h1>
