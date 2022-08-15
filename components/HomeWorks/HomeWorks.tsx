479bc67128161d4093b58663cbc7b7cb68ba5802
kinsyu <miguelangelrm31@gmail.com>
2022-08-15 18:30:54 +0100

    autocompounding

--- components/HomeWorks/HomeWorks.tsx
+++ components/HomeWorks/HomeWorks.tsx
@@ -15,7 +15,7 @@ export default function HomeContent() {
         <div data-aos="fade-left" className={styles.rightWork}>
           <h1>HOW IT WORKS</h1>
           <p>
-            <span className={styles.bold}>Auto-Compouding Protocol</span> <br />
+            <span className={styles.bold}>AutoCompouding Protocol</span> <br />
             ProtocolX $PTX begins as a 1.77% daily auto-rebasing token.{' '}
             <span className={styles.bold}>What does that mean?</span> Once you buy the token, it
             will begin to automatically rebase in your wallet. No work to do to make the magic
